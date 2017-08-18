478131efc1d94b8dccfb49dbef17d90c4fd2e633
Ryo Ota <nwtgck@gmail.com>
2017-08-18 14:02:50 +0900

    [impl] Implement SET_VISIBILITY_FILTER on view

--- src/actionCreators.ts
+++ src/actionCreators.ts
@@ -5,5 +5,5 @@ const actionCreator = actionCreatorFactory();
 export const addTodo             = actionCreator<{id: number, text: string}>('ADD_TODO');
 export const toggleTodo          = actionCreator<{id: number}>('TOGGLE_TODO');
 
-export type Filter = "SHOW_ALL";
+export type Filter = 'SHOW_ALL' | 'SHOW_ACTIVE' | 'SHOW_COMPLETED';
 export const setVisibilityFilter = actionCreator<{filter: Filter}>("SET_VISIBILITY_FILTER");
--- src/index.tsx
+++ src/index.tsx
@@ -6,24 +6,50 @@ import {TodoItem}      from "./datatypes"
 
 import {todoAppReducer} from "./reducers";
 import * as actionCreator from './actionCreators';
+import {Filter} from "./actionCreators";
+import {ReactNode} from "react";
 
 const store = createStore(todoAppReducer);
 
-// Typed Props
-interface Props {
-  todos: List<TodoItem>
+class FilterLink extends React.Component<{filter: Filter, currentFilter: Filter, children: ReactNode}, {}> { // (ReactNode from: https://github.com/Microsoft/TypeScript/issues/6471)
+    render (){
+        if(this.props.filter === this.props.currentFilter){
+            return <span>{this.props.children}</span>
+        } else {
+            return (
+                <a href="#" onClick={e => {
+                    e.preventDefault();
+                    store.dispatch(actionCreator.setVisibilityFilter({filter: this.props.filter}))
+                }}>{this.props.children}</a>
+            );
+        }
+
+    }
 }
 
-// Typed State
-interface State {
-}
 
+
+const getVisibleTodos = (todos: List<TodoItem>, filter: Filter) => {
+    switch (filter){
+        case 'SHOW_ALL':
+            return todos;
+        case 'SHOW_ACTIVE':
+            return todos.filter(todo => !todo.completed);
+        case 'SHOW_COMPLETED':
+            return todos.filter(todo => todo.completed);
+    }
+};
 let nextTodId = 0;
-class TodoApp extends React.Component<Props, State> {
+class TodoApp extends React.Component<{todos: List<TodoItem>, visibilityFilter: Filter}, {}> {
 
   input = null;
 
   render () {
+    const {todos, visibilityFilter} = this.props;
+    const visibleTodos = getVisibleTodos(
+        todos,
+        visibilityFilter
+    );
     return (
       <div>
         <input ref={node => this.input = node}/>
@@ -35,7 +61,7 @@ class TodoApp extends React.Component<Props, State> {
             this.input.value = '';
           }}>Add Todo</button>
         <ul>
-            {this.props.todos.map(todo =>
+            {visibleTodos.map(todo =>
                 <li key={todo.id}
                     onClick={() => {
                         store.dispatch(actionCreator.toggleTodo({
@@ -51,13 +77,35 @@ class TodoApp extends React.Component<Props, State> {
                 </li>
             )}
         </ul>
+      <p>
+          Show:
+          {' '}
+          <FilterLink
+            filter       ={'SHOW_ALL'}
+            currentFilter={visibilityFilter}
+          >All</FilterLink>
+          {' '}
+          <FilterLink
+              filter       ={'SHOW_ACTIVE'}
+              currentFilter={visibilityFilter}
+          >Active</FilterLink>
+          {' '}
+          <FilterLink
+              filter       ={'SHOW_COMPLETED'}
+              currentFilter={visibilityFilter}
+          >Completed</FilterLink>
+          {' '}
+      </p>
       </div>
     );
   }
 }
 
 const render = () => {
-    ReactDOM.render(<TodoApp todos={store.getState().todos}/>, document.getElementById('root'));
+    ReactDOM.render(<TodoApp
+        todos={store.getState().todos}
+        visibilityFilter={store.getState().visibilityFilter}
+    />, document.getElementById('root'));
 };
 
 store.subscribe(render);
